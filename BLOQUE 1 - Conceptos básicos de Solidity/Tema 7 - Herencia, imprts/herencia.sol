c74ac7d5fc9cc61c9a011f58bb309ebf3d89e055
Joan73 <jponte98@gmail.com>
2021-08-01 17:20:55 +0200

    cambio

--- BLOQUE 1 - Conceptos básicos de Solidity/Tema 7 - Herencia, imprts/herencia.sol
+++ BLOQUE 1 - Conceptos básicos de Solidity/Tema 7 - Herencia, imprts/herencia.sol
@@ -1,18 +1,23 @@
+//Indicamos la version
 pragma solidity >=0.4.4 <0.7.0;
 
 contract Banco{
     
+    //Definimos un tipo de dato complejo cliente
     struct cliente{
-        string nombre;
+        string _nombre;
+        address direccion;
         uint dinero;
     }
     
-    mapping(string => cliente) clientes; 
+    //mapping que nos relacionar el nombre del cliente con el tipo de dato cliente
+    mapping (string => cliente) clientes;
+    
+    //Funcion que nos permita dar de alta un nuevo cliente
     
     function nuevoCliente(string memory _nombre) public {
-        clientes[_nombre] = cliente(_nombre, 0);
+        clientes[_nombre] = cliente(_nombre, msg.sender, 0);
     }
-    
 }
 
 contract Cliente is Banco{
@@ -21,21 +26,23 @@ contract Cliente is Banco{
         nuevoCliente(_nombre);
     }
     
-    function IngresarDinero(string memory _nombre, uint _cantidad) public {
+    function IngresarDinero(string memory _nombre, uint _cantidad) public{
         clientes[_nombre].dinero = clientes[_nombre].dinero + _cantidad;
     }
     
-    function RetirarDinero(string memory _nombre, uint _cantidad) public returns(bool, uint){
-       
-        bool flag=true;
+    function RetirarDinero(string memory _nombre, uint _cantidad) public returns(bool){
+        bool flag = true;
         
-        if(clientes[_nombre].dinero-_cantidad>0){
-            clientes[_nombre].dinero = clientes[_nombre].dinero + _cantidad;
+        if(int(clientes[_nombre].dinero)-int(_cantidad) >= 0){
+            clientes[_nombre].dinero = clientes[_nombre].dinero - _cantidad;
         }else{
-            flag=false;
+            flag = false;
         }
         
-        return(flag, clientes[_nombre].dinero);
+        return flag;
     }
     
+    function ConsultarDinero(string memory _nombre) public view returns(uint){
+        return clientes[_nombre].dinero;
+    }
 }
